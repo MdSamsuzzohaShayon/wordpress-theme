49bc3215212e790e174cb9f9163af5e31f5afe9c
Md Samsuzzoha <mdshayon0@gmail.com>
2022-03-16 05:49:37 +0600

    stracture a basic theme

---
+++ front-page.php
@@ -0,0 +1,42 @@
+<?php get_header(); ?>
+<!-- <div class="main-header-body-wrapper" style="background-image: url(<?php //echo get_theme_mod("thesportworship_banner"); ?>);"> -->
+
+
+
+
+<!-- MAIN SECTION START  -->
+<main class="main-body background-transparent">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-12 text-white">
+                <h2 class="text-uppercase heading-caption">TItle Title goes here</h2>
+                <p >Description</p>
+            </div>
+
+        </div>
+    </div>
+</main>
+<!-- MAIN SECTION ENDS  -->
+
+
+</div>
+<!--
+<?php
+// if(get_theme_mod('author-callout-dispay') === 'Yes'){ ?>
+<h2>Uploaded text</h2>
+<img src="<?php  //echo wp_get_attachment_url(get_theme_mod('author-callout-image')); ?>" class="img-thumbnail" alt="">
+<div class="text-error">
+  <?php
+// $authortext = get_theme_mod('author-callout-text');
+// if($authortext != ''){
+//   echo $authortext;
+// }else{
+//   echo "Add text for auth";
+// }
+?>
+</div>
+ <?php
+//}
+?>
+abc -->
+<?php get_footer(); ?>
---
+++ functions.php
@@ -0,0 +1,72 @@
+
+<?php
+
+if ( function_exists( 'add_theme_support' ) ) {
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support('title-tag');
+    add_theme_support( 'post-thumbnails', array( 'carousel' ) );
+    set_post_thumbnail_size( 150, 150, true ); // default Featured Image dimensions (cropped)
+
+    // additional image sizes
+    // delete the next line if you do not need additional image sizes
+    add_image_size( 'category-thumb', 300, 9999 ); // 300 pixels wide (and unlimited height)
+    add_theme_support('custom-header', array(      "default-image" => get_template_directory_uri() . '/assets/img/default-bg.jpg'  ));
+}
+
+
+
+
+
+// ENQUEUE
+function thesportworship_enqueue_style(){
+    wp_enqueue_style( 'stylesheet', get_stylesheet_uri());
+    wp_enqueue_style( 'custom-bootstrap-and-main', get_theme_file_uri("/assets/css/style.css"), array(), '1.0.0' );
+    // <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    // wp_enqueue_style( 'bootstrap-icons-svg', get_theme_file_uri("/assets/icon/bootstrap-icons/bootstrap-icons.svg"), array(), '1.0.0' );
+    wp_enqueue_style( 'bellefier-font', get_theme_file_uri("/assets/fonts/Bellefair-Regular.ttf"), array(), '1.0.0' );
+    wp_enqueue_style( 'bebas-neue-font', get_theme_file_uri("/assets/fonts/BebasNeue-Regular.ttf"), array(), '1.0.0' );
+    wp_enqueue_style( 'bootstrap-icons', get_theme_file_uri("/assets/icon/bootstrap-icons/bootstrap-icons.css"), array(), '1.0.0' );
+}
+add_action("wp_enqueue_scripts", "thesportworship_enqueue_style");
+
+
+
+function thesportworship_enqueue_script(){
+    // /opt/lampp/htdocs/thesportworship-sport/wp-content/themes/thesportworship-sport-theme/assets/bootstrap/dist/js/bootstrap.min.js
+    wp_enqueue_script( 'bootstrap-js', get_theme_file_uri("/assets/bootstrap/dist/js/bootstrap.bundle.min.js"), array(), '5.0.0' );
+    wp_enqueue_script( 'main-js', get_theme_file_uri("/assets/js/main.js"), array(), '1.0.0' );
+}
+add_action("wp_enqueue_scripts", "thesportworship_enqueue_script");
+
+
+
+
+// require_once(get_template_directory() . "/inc/customize-author-detail.php");
+// new TheSportWorship_Author_Customizer();
+
+
+
+// Customize logo and menu position
+require_once(get_template_directory() . "/inc/customize-menu-position.php");
+
+require_once(get_template_directory() . "/inc/customize-header-caption.php");
+
+
+
+// MAIN MENU WITH FILTERS - main-menu-filter-register
+require_once(get_template_directory() . "/inc/main-menu-filter-register.php");
+
+
+require_once(get_template_directory() . "/inc/top-menu-walker-class.php");
+
+
+
+
+
+
+
+
+
+
+
+?>
